7d724978ee0e073f283abac8877a9eb8888f8406
viduxsh <39436250+viduxsh@users.noreply.github.com>
2022-07-20 01:01:04 +0200

    Added footer
    Added footer and other small things

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,9 @@
-const input=document.querySelector(".theme-switcher input");
+const input = document.querySelector(".theme-switcher input");
 
-input.addEventListener("change", (e)=>{
-  if(e.target.checked){
+input.addEventListener("change", (e) => {
+  if (e.target.checked) {
     document.body.setAttribute("data-theme", "dark");
-  }
-  else{
+  } else {
     document.body.setAttribute("data-theme", "light");
   }
 });
